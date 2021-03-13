430f7292a0acc7d2663f604a394ad4ab291f4087
Bekir-Berke <bberkey50@gmail.com>
2021-03-14 00:12:40 +0300

    13.03.2021

--- DataAccess/Concrete/EntityFramework/EFCategoryDal.cs
+++ DataAccess/Concrete/EntityFramework/EFCategoryDal.cs
@@ -10,29 +10,6 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
     {
-        public void Add(Category entity)
-        {
-            throw new NotImplementedException();
-        }
 
-        public void Delete(Category entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Category Get(Expression<Func<Category, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Category entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
